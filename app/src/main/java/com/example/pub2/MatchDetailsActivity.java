8eb2f9e47de77f82a879b4be830880a57ec42683
Hashim PC <highbrowdirector@gmail.com>
2019-04-18 11:39:59 +0530

    from pc to lap

--- app/src/main/java/com/example/pub2/MatchDetailsActivity.java
+++ app/src/main/java/com/example/pub2/MatchDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.example.pub2;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -26,8 +28,17 @@ public class MatchDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_match_details);
 
         slotview = findViewById(R.id.slotview);
-        String match_id = getIntent().getStringExtra("match_id");
+        final String match_id = getIntent().getStringExtra("match_id");
         slotview.setText(slot);
+
+        slotview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MatchDetailsActivity.this , ParticipantsActivity.class);
+                intent.putExtra("match_id", match_id);
+                startActivity(intent);
+            }
+        });
 
         databaseReference.child(match_id).addValueEventListener(new ValueEventListener() {
             @Override
